2f9c0fcaacb4a836246d8f351ec473f14b2d1c45
alexanderushe <alex.f.ushe@gmail.com>
2023-12-09 00:30:12 +0200

    utils

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 function controllerRouting(app) {
   const router = express.Router();
@@ -15,6 +16,10 @@ function controllerRouting(app) {
   // should return the number of users and files in DB
   router.get('/stats', (req, res) => {
     AppController.getStats(req, res);
+  });
+
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
   });
 }
 
